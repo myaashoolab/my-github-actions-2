80c35d2ce96a03df79f5d4ca6e08ec0d0ddf7766
myaashoolab <myaashoolab@gmail.com>
2023-08-17 02:51:19 +0530

    hello docker

--- demo.js
+++ demo.js
@@ -1,2 +1,28 @@
-const currentDate = new Date().toLocaleDateString();
-console.log(`Hello, World! Today's date is ${currentDate}`);
+// const currentDate = new Date().toLocaleDateString();
+// console.log(`Hello, World! Today's date is ${currentDate}`);
+const Appwrite = require('appwrite');
+
+const client = new Appwrite();
+
+client.setEndpoint("https://cloud.appwrite.io/v1");
+client.setProject("64dd3509988a3c15ab1c");
+client.setKey(process.env.APPWRITE_API_KEY);
+
+const database = new Appwrite.Database(client);
+
+const collectionId = 'your-collection-id'; // Replace with your actual collection ID
+
+async function createDocument() {
+  try {
+    const response = await database.createDocument(collectionId, {
+      message: 'Hello, World!',
+      timestamp: new Date().toISOString(),
+    });
+
+    console.log('Document created:', response);
+  } catch (error) {
+    console.error('Error creating document:', error);
+  }
+}
+
+createDocument();
